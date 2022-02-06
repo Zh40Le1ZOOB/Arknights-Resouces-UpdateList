4277de8e8ab00424bcb4d1beefe8ae6030fb1ed0
jxr2006 <65601588+jxr2006@users.noreply.github.com>
2022-02-06 15:46:17 +0800

    Update update.py

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import oss2, json
 with open('H:/RhodeIslandRT/oss_res_table.json', 'r', encoding='utf-8') as f1, open('H:/RhodeIslandRT/activity_table.json', 'r', encoding='utf-8') as f2:
     oss_res_table: dict = json.load(f1)
     activity_table: dict = json.load(f2)
-bucket = oss2.Bucket(oss2.AnonymousAuth(), 'http://{}'.format(oss_res_table['ossServerEndPoint']), 'arknights-static')
+bucket = oss2.Bucket(oss2.AnonymousAuth(), 'http://{}'.format(oss_res_table['ossServerEndPoint']), oss_res_table['ossServerBucket'])
 for activity in oss_res_table['activities'].keys():
     name = activity_table['basicInfo'][activity]['name'] if activity in activity_table['basicInfo'].keys() else None
     oss_res_table['activities'][activity]['name'] = name
